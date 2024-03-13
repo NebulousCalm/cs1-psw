caf40ed91631106527f2bd573669a49a3790cee1
Nebulous <88895551+NebulousCalm@users.noreply.github.com>
2024-03-13 08:18:18 -0400

    Update dependents.sh

--- dependents.sh
+++ dependents.sh
@@ -1,4 +1,5 @@
 echo "Make sure the Node.js runtime is installed and so is the npm package manager."
 
 npm install express
-npm install path
+npm install path # should be installed by default could ignore
+npm install ejs
